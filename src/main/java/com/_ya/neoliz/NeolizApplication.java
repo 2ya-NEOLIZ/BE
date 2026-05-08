e34ceca938074a7771e1d241cc0f12582f106504
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-05-09 03:06:25 +0900

    chore: Spring Boot 프로젝트 초기 구조 추가

---
+++ src/main/java/com/_ya/neoliz/NeolizApplication.java
@@ -0,0 +1,13 @@
+package com._ya.neoliz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeolizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeolizApplication.class, args);
+	}
+
+}
