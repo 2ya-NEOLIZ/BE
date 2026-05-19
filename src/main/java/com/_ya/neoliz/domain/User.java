63fed0313c9902806be997836ccc636bcdea35c5
kjw <zw7977@gmail.com>
2026-05-19 18:36:12 +0900

    feature: User Entity 구현

---
+++ src/main/java/com/_ya/neoliz/domain/User.java
@@ -0,0 +1,27 @@
+package com._ya.neoliz.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private String nickname;
+
+    @Column(nullable = false, length = 500)
+    private String profileImageUrl;
+}
