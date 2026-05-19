1a55ec9908e92908557903d276515d9b369e125a
kjw <zw7977@gmail.com>
2026-05-19 18:48:08 +0900

    feature: 내 정보 조회 API 추가

---
+++ src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
@@ -0,0 +1,27 @@
+package com._ya.neoliz.presentation.controller;
+
+import com._ya.neoliz.application.service.ProfileService;
+import com._ya.neoliz.presentation.dto.response.UserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final ProfileService profileService;
+    @Operation(
+            summary = "내 정보 조회",
+            description = "내 정보를 조회합니다."
+    )
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> findById(@PathVariable Long id) {
+        UserResponse response = profileService.findById(id);
+        return ResponseEntity.ok(response);
+    }
+}
