79c3fedde0a9c6e98c88b3afc1829d3c63f63222
kjw <zw7977@gmail.com>
2026-05-22 01:59:53 +0900

    fix: 유저 조회 실패 시 404 예외 발생 적용

--- src/main/java/com/_ya/neoliz/application/service/ProfileService.java
+++ src/main/java/com/_ya/neoliz/application/service/ProfileService.java
@@ -1,6 +1,7 @@
 package com._ya.neoliz.application.service;
 
 import com._ya.neoliz.domain.User;
+import com._ya.neoliz.global.exception.UserNotFound;
 import com._ya.neoliz.persistence.repository.UserRepository;
 import com._ya.neoliz.presentation.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProfileService {
     private final UserRepository userRepository;
     public UserResponse findById(Long id){
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. id = "+id));
-        return UserResponse.builder()
-                .id(user.getId())
-                .email(user.getEmail())
-                .nickname(user.getNickname())
-                .profileImageUrl(user.getProfileImageUrl())
-                .build();
+        User user =  userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFound("조회 실패"));
+        return UserResponse.from(user);
     }
 }
