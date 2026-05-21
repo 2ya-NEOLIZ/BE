042911a798abc2caee3736b7a2d9603b1cdb61f0
kjw <zw7977@gmail.com>
2026-05-22 02:05:16 +0900

    fix: GetMapping 경로 및 파라미터 매핑 수정

--- src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
+++ src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
@@ -5,10 +5,7 @@ import com._ya.neoliz.global.response.ApiResponse;
 import com._ya.neoliz.presentation.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
@@ -20,8 +17,8 @@ public class UserController {
             summary = "내 정보 조회",
             description = "내 정보를 조회합니다."
     )
-    @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> getProfile(@PathVariable Long id) {
+    @GetMapping("me")
+    public ResponseEntity<?> getProfile(@RequestParam("id") Long id) {
         UserResponse userResponse = profileService.findById(id);
         return ResponseEntity.ok(ApiResponse.success("조회 완료", userResponse));
     }
