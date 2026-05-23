72ce88d2ad4ece2a93000645c26b2dde59aeb622
kjw <zw7977@gmail.com>
2026-05-24 00:15:09 +0900

    feature: 사용자 id 기반 시퀀스 페이징 조회 메서드 추가

--- src/main/java/com/_ya/neoliz/persistence/repository/SequenceRepository.java
+++ src/main/java/com/_ya/neoliz/persistence/repository/SequenceRepository.java
@@ -1,8 +1,11 @@
 package com._ya.neoliz.persistence.repository;
 
 import com._ya.neoliz.domain.Sequence;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SequenceRepository extends JpaRepository<Sequence, Long> {
+    Page<Sequence> findAllByUserId(Long id, Pageable pageable);
 
 }
