bbf89146aa4a2604a6517783cee95cebc6b5d091
Kim Jiwoo <zw7977@gmail.com>
2026-05-23 22:20:24 +0900

    [BE-#6] 내 정보 조회 API 구현 완료 (#8)
    * feature: User Entity 구현
    
    * feature: UserRepository 구현
    
    * feature: 유저 프로필 조회 로직 구현
    
    * feature: 사용자 프로필 반환을 위한 UserResponse DTO 추가
    
    * feature: 내 정보 조회 API 추가
    
    * fix: springdoc-openapi v2.5.0 의존성 추가
    
    * feature: Builder 및 정적 팩토리 메서드 구현
    
    * refactor: API 엔드포인트 변경 및 Swagger 응답 포맷 동기화
    
    * feature: 전역 예외 핸들러&404 처리 기능 구현
    
    * feature: UserNotFound 예외 클래스 추가
    
    * fix: 유저 조회 실패 시 404 예외 발생 적용
    
    * fix: GetMapping 경로 및 파라미터 매핑 수정
    
    * fix: 매핑 경로 수정 & getProfile 메서드 id 파라미터 제거
    
    * refactor: 예외 클래스 이름 변경

---
+++ src/main/java/com/_ya/neoliz/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com._ya.neoliz.global.exception;
+
+import com._ya.neoliz.global.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException unf) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ApiResponse.error("F404", unf.getMessage(), null));
+    }
+}
---
+++ src/main/java/com/_ya/neoliz/persistence/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com._ya.neoliz.persistence.repository;
+
+import com._ya.neoliz.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
