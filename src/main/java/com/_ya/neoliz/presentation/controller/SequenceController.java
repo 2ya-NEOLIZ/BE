f7732217f1c8fc5c746996b030756db5b9c43304
kjw <zw7977@gmail.com>
2026-05-24 02:28:48 +0900

    chore: @Tag, @Operation 추가

--- src/main/java/com/_ya/neoliz/presentation/controller/SequenceController.java
+++ src/main/java/com/_ya/neoliz/presentation/controller/SequenceController.java
@@ -3,6 +3,8 @@ package com._ya.neoliz.presentation.controller;
 import com._ya.neoliz.application.service.SequenceService;
 import com._ya.neoliz.global.response.ApiResponse;
 import com._ya.neoliz.presentation.dto.response.SequencePageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
@@ -12,13 +14,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Tag(name = "시퀀스", description = "시퀀스 관련 API")
 @RestController
 @RequestMapping("/api/v1/neoliz/users")
 @RequiredArgsConstructor
 public class SequenceController {
     private final SequenceService sequenceService;
     @GetMapping("/me/sequences")
+    @Operation(summary = "시퀀스 목록 조회", description = "마이페이지에서 사용자의 시퀀스 목록을 페이징하여 조회합니다.")
     public ResponseEntity<ApiResponse<SequencePageResponse>> getSequence(@ParameterObject @PageableDefault(size = 4, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         Long id = 1L; // getSequenceList()를 위한 임시 데이터
         SequencePageResponse response = sequenceService.getSequenceList(id, pageable);
