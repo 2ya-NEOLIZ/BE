a66728e291e9289bc98e7d8f92379c7c93e1d3c8
kjw <zw7977@gmail.com>
2026-05-25 02:06:13 +0900

    feature: 시퀀스 상세 조회 API 구현

--- src/main/java/com/_ya/neoliz/presentation/controller/SequenceController.java
+++ src/main/java/com/_ya/neoliz/presentation/controller/SequenceController.java
@@ -2,6 +2,7 @@ package com._ya.neoliz.presentation.controller;
 
 import com._ya.neoliz.application.service.SequenceService;
 import com._ya.neoliz.global.response.ApiResponse;
+import com._ya.neoliz.presentation.dto.response.SequenceDetailResponse;
 import com._ya.neoliz.presentation.dto.response.SequencePageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -26,6 +28,14 @@ public class SequenceController {
     @Operation(summary = "시퀀스 목록 조회", description = "마이페이지에서 사용자의 시퀀스 목록을 페이징하여 조회합니다.")
     public ResponseEntity<ApiResponse<SequencePageResponse>> getSequence( @AuthenticationPrincipal Long userId, @ParameterObject @PageableDefault(size = 4, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         SequencePageResponse response = sequenceService.getSequenceList(userId, pageable);
+        return ResponseEntity.ok(ApiResponse.success("조회 완료", response));
+    }
+
+    @GetMapping("/me/sequences/{sequenceId}")
+    @Operation(summary = "시퀀스 상세 조회", description = "마이페이지에서 사용자의 상세 시퀀스를 조회합니다.")
+    public ResponseEntity<ApiResponse<SequenceDetailResponse>> getSequenceDetail(@PathVariable("sequenceId") Long sequenceId) {
+        Long userId = 1L; // getSequenceDetail()을 위한 임시 데이터
+        SequenceDetailResponse response = sequenceService.getSequenceDetail(userId, sequenceId);
         return ResponseEntity.ok(ApiResponse.success("조회 완료", response));
     }
 }
