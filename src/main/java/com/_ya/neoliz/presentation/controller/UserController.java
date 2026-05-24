54ab44b7623bb3660e2a2c8b5abcc35b607daeb7
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-05-25 00:15:13 +0900

    feat: UserController에 Swagger @Tag 어노테이션 추가

--- src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
+++ src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
@@ -3,12 +3,14 @@ package com._ya.neoliz.presentation.controller;
 import com._ya.neoliz.application.service.ProfileService;
 import com._ya.neoliz.global.response.ApiResponse;
 import com._ya.neoliz.presentation.dto.response.UserResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
+@Tag(name = "사용자", description = "사용자 정보 관련 API")
 @RestController
 @RequestMapping("/api/v1/neoliz/users")
 @RequiredArgsConstructor
