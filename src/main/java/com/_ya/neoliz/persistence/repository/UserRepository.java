13d47f76204fb251fcbb09d31cffe96ac51847f0
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-05-24 22:07:15 +0900

    refactor: UserRepository.java에 중복 검증 메서드 추가

--- src/main/java/com/_ya/neoliz/persistence/repository/UserRepository.java
+++ src/main/java/com/_ya/neoliz/persistence/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com._ya.neoliz.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
