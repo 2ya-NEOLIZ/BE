0a567c8cdc9506b8b77ad93e64f9fd439163cdd8
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-05-24 23:38:55 +0900

    feat: 리프레시 토큰 조회를 위한 RefreshTokenRepository 추가

---
+++ src/main/java/com/_ya/neoliz/persistence/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com._ya.neoliz.persistence.repository;
+
+import com._ya.neoliz.domain.RefreshToken;
+import com._ya.neoliz.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUser(User user);
+}
