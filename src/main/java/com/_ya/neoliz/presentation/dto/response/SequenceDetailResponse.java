3d6c04dc94f96e2d1a295406c585dbf5bcea3641
kjw <zw7977@gmail.com>
2026-05-25 02:04:26 +0900

    feature: 시퀀스 상세 정보 응답용 DTO 생성

---
+++ src/main/java/com/_ya/neoliz/presentation/dto/response/SequenceDetailResponse.java
@@ -0,0 +1,25 @@
+package com._ya.neoliz.presentation.dto.response;
+
+import com._ya.neoliz.domain.Sequence;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class SequenceDetailResponse {
+    private Long id;
+    private String title;
+    private List<SequenceItemsResponse> items;
+
+    public static SequenceDetailResponse from(Sequence sequence, List<SequenceItemsResponse> itemsList) {
+        return SequenceDetailResponse.builder()
+                .id(sequence.getId())
+                .title(sequence.getTitle())
+                .items(itemsList)
+                .build();
+    }
+
+}
