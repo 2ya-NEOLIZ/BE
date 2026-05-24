8dfca18d0c05529d1c10c0f5220f3b3850489d77
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-05-25 00:37:13 +0900

    feat: 로그아웃을 위한 deleteByUserId 메서드 추가

--- src/main/java/com/_ya/neoliz/persistence/repository/RefreshTokenRepository.java
+++ src/main/java/com/_ya/neoliz/persistence/repository/RefreshTokenRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
     Optional<RefreshToken> findByUser(User user);
+    void deleteByUserId(Long userId);
+
 }
