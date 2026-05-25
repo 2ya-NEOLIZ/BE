6f2faade3d332b6bb29670c0270727ad79b87c57
김주희 <juhui@gimjuhuiui-MacBookPro.local>
2026-05-26 03:29:10 +0900

    feat: EmojiNotFoundException 추가

---
+++ src/main/java/com/_ya/neoliz/global/exception/EmojiNotFoundException.java
@@ -0,0 +1,7 @@
+package com._ya.neoliz.global.exception;
+
+public class EmojiNotFoundException extends RuntimeException {
+    public EmojiNotFoundException(String message) {
+        super(message);
+    }
+}
