026db13bf8ba90f281dec538b28559cd66442537
kjw <zw7977@gmail.com>
2026-05-27 01:26:56 +0900

    feature: 닉네임 수정 로직 구현

--- src/main/java/com/_ya/neoliz/application/service/ProfileService.java
+++ src/main/java/com/_ya/neoliz/application/service/ProfileService.java
@@ -1,8 +1,11 @@
 package com._ya.neoliz.application.service;
 
 import com._ya.neoliz.domain.User;
+import com._ya.neoliz.global.exception.DuplicateNicknameException;
 import com._ya.neoliz.global.exception.UserNotFoundException;
 import com._ya.neoliz.persistence.repository.UserRepository;
+import com._ya.neoliz.presentation.dto.request.UpdateNicknameRequest;
+import com._ya.neoliz.presentation.dto.response.UpdateNicknameResponse;
 import com._ya.neoliz.presentation.dto.response.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,12 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class ProfileService {
     private final UserRepository userRepository;
+    @Transactional(readOnly = true)
     public UserResponse findById(Long id){
         User user =  userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException("조회 실패"));
         return UserResponse.from(user);
+    }
+
+    @Transactional
+    public UpdateNicknameResponse updateNickname(Long id, UpdateNicknameRequest request) {
+        String newNickname = request.getNickname();
+        if (userRepository.existsByNickname(newNickname)) { throw new DuplicateNicknameException("닉네임 중복"); }
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("조회 실패"));
+        user.updateNickname(newNickname);
+        return UpdateNicknameResponse.from(user);
     }
 }
