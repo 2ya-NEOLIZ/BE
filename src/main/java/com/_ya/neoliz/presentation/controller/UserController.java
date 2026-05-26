75b207297b085129d32186becccda18aa1c4de46
kjw <zw7977@gmail.com>
2026-05-27 01:27:34 +0900

    feature: 닉네임 수정 API 구현

--- src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
+++ src/main/java/com/_ya/neoliz/presentation/controller/UserController.java
@@ -2,8 +2,11 @@ package com._ya.neoliz.presentation.controller;
 
 import com._ya.neoliz.application.service.ProfileService;
 import com._ya.neoliz.global.response.ApiResponse;
+import com._ya.neoliz.presentation.dto.request.UpdateNicknameRequest;
+import com._ya.neoliz.presentation.dto.response.UpdateNicknameResponse;
 import com._ya.neoliz.presentation.dto.response.UserResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +27,15 @@ public class UserController {
     public ResponseEntity<?> getProfile(@AuthenticationPrincipal Long userId) {
         UserResponse userResponse = profileService.findById(userId);
         return ResponseEntity.ok(ApiResponse.success("조회 완료", userResponse));
+    }
+
+    @Operation(
+            summary = "닉네임 수정",
+            description = "내 닉네임을 수정합니다."
+    )
+    @PatchMapping("/me/nickname")
+    public ResponseEntity<ApiResponse<UpdateNicknameResponse>> updateNickname(@AuthenticationPrincipal Long userId, @RequestBody @Valid UpdateNicknameRequest request) {
+        UpdateNicknameResponse nicknameResponse =  profileService.updateNickname(userId, request);
+        return ResponseEntity.ok(ApiResponse.success("닉네임 변경 성공", nicknameResponse));
     }
 }
